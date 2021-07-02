03a97d72968ce9eb4f791c02eb6fccad5504353c
techtangents <167879+techtangents@users.noreply.github.com>
2021-07-02 12:03:41 +1000

    export

--- src/ts/Session1.ts
+++ src/ts/Session1.ts
@@ -1,34 +1,34 @@
 import {Pair, todo} from './Data';
 
-const e01 = <A> (a: A): A => todo();
+export const e01 = <A> (a: A): A => todo();
 
-const e02 = <A, B> (a: A, b: B): A => todo();
+export const e02 = <A, B> (a: A, b: B): A => todo();
 
-const e03 = <A, B> (a: A, b: B): B => todo();
+export const e03 = <A, B> (a: A, b: B): B => todo();
 
-const e04 = <A, B> (a: A, b: B): Pair<A, B> => todo();
+export const e04 = <A, B> (a: A, b: B): Pair<A, B> => todo();
 
-const e05 = <A> (a: A): Pair<A, A> => todo();
+export const e05 = <A> (a: A): Pair<A, A> => todo();
 
 // hint: destructure params in the param lists
 
-const e06 = <A, B> (p: Pair<A, B>): A => todo();
+export const e06 = <A, B> (p: Pair<A, B>): A => todo();
 
-const e07a = <A, B> (p: Pair<A, B>): A => todo();
+export const e07a = <A, B> (p: Pair<A, B>): A => todo();
 
-const e07b = <A, B> (p: Pair<A, B>): B => todo();
+export const e07b = <A, B> (p: Pair<A, B>): B => todo();
 
-const e08 = <A, B> (p: Pair<A, B>): Pair<A, B> => todo();
+export const e08 = <A, B> (p: Pair<A, B>): Pair<A, B> => todo();
 
-const e09 = <A, B> (p: Pair<A, B>): Pair<B, A> => todo();
+export const e09 = <A, B> (p: Pair<A, B>): Pair<B, A> => todo();
 
-const e10a = <A, B, C> (p: Pair<Pair<A, B>, C>): A => todo();
+export const e10a = <A, B, C> (p: Pair<Pair<A, B>, C>): A => todo();
 
-const e10b = <A, B, C> (p: Pair<Pair<A, B>, C>): B => todo();
+export const e10b = <A, B, C> (p: Pair<Pair<A, B>, C>): B => todo();
 
-const e10c = <A, B, C> (p: Pair<Pair<A, B>, C>): C => todo();
+export const e10c = <A, B, C> (p: Pair<Pair<A, B>, C>): C => todo();
 
-const e11 = <A, B, C> (p: Pair<Pair<A, B>, C>): Pair<A, C> => todo();
+export const e11 = <A, B, C> (p: Pair<Pair<A, B>, C>): Pair<A, C> => todo();
 
-const e12 = <A, B, C> (p: Pair<Pair<A, B>, C>): Pair<A, Pair<B, C>> => todo();
+export const e12 = <A, B, C> (p: Pair<Pair<A, B>, C>): Pair<A, Pair<B, C>> => todo();
 
